037eaf7224812795c2665934fb2d30a9ce51585e
Alvaro Zerbini - Alpar <alvaro.zerbini@alpar.com.br>
2025-09-30 16:42:16 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
+
   const port = parseInt(process.env.PORT ?? '8080', 10);
   await app.listen(port, '0.0.0.0');
-  Logger.log(`API ouvindo em :${port}`);
 }
 bootstrap();
