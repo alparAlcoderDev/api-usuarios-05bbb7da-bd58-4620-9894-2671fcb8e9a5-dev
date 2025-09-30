dd6d992b8702863fce5303a7b78f7620245d74b4
Alvaro Zerbini - Alpar <alvaro.zerbini@alpar.com.br>
2025-09-30 16:46:17 -0300

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, DeepPartial } from 'typeorm';
 import { User } from '../entities/user.entity';        
-import { CreateUserDto } from '../dto/user.dto.ts';       
+import { CreateUserDto } from '../dto/user.dto';       
 
 @Injectable()
 export class UserService {
